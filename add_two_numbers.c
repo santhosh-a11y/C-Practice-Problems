9ed149b40d12fb96108ac5d98d28f48cd166249d
santhosh <santhosh7kumar7u77@gmail.com>
2024-12-15 18:51:09 +0530

    Add files via upload

---
+++ add_two_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	
+	//sum of two numbers
+	int a=4, b=8;
+	
+	//a variable to store the sum of the two numbers
+	int c=a+b;
+	
+	//printing the sum
+	printf("%d", c);
+
+}
+
+
